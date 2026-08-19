dffdfa5189210347d9808d1404d766b28672504b
euphor82 <euphor82@gmail.com>
2026-08-19 17:35:30 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'rooted-v36';
+const CACHE = 'rooted-v37';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
